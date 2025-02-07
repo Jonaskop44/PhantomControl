fd937f16e07fadb612cf15099e508f2cafdfcb3f
jonaskop44 <jonas@codeflexx.com>
2025-02-07 19:54:58 +0100

    changes

--- client/src/command_handler.py
+++ client/src/command_handler.py
@@ -1,8 +1,10 @@
 import subprocess
+import os
 
 def run_command(command):
     try:
-        result = subprocess.run(command, shell=True, capture_output=True, text=True)
+        desktop_path = os.path.join(os.path.expanduser("~"), "Desktop")
+        result = subprocess.run(command, shell=True, capture_output=True, text=True, cwd=desktop_path)
         response = result.stdout if result.stdout else result.stderr
         print(f"[Command output]: \n{response}")
         return response
