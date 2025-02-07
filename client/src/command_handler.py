fc171df82f1283d044672630fcae2f12be1877b8
jonaskop44 <jonas@codeflexx.com>
2025-02-07 19:41:26 +0100

    changes

--- client/src/command_handler.py
+++ client/src/command_handler.py
@@ -2,7 +2,6 @@ import subprocess
 
 def run_command(command):
     try:
-        #test
         result = subprocess.run(command, shell=True, capture_output=True, text=True)
         response = result.stdout if result.stdout else result.stderr
         print(f"[Command output]: \n{response}")
