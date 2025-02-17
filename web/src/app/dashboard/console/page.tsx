6bbcbdef15d1302efaac217fdd4614d60499929d
jonaskop44 <jonas@codeflexx.com>
2025-02-17 23:16:55 +0100

    changes

--- web/src/app/dashboard/console/page.tsx
+++ web/src/app/dashboard/console/page.tsx
@@ -1,13 +1,13 @@
 "use client";
 
-import { Tabs, Tab } from "@heroui/tabs";
 import ApiClient from "@/api";
 import { Consoles } from "@/types/consoles";
 import { useEffect, useState } from "react";
 import { Icon } from "@iconify/react";
-import { Chip, Input, Spinner } from "@heroui/react";
+import { Avatar, Chip, Input, Button, Spinner } from "@heroui/react";
 import { toast } from "sonner";
 import { Messages } from "@/types/message";
+import clsx from "clsx";
 
 const apiClient = new ApiClient();
 
@@ -41,6 +41,7 @@ const ConsolePage = () => {
         .then((response) => {
           if (response.status && response.data?.messages) {
             setMessages(response.data.messages);
+            console.log(response.data.messages);
           } else {
             setMessages([]);
           }
@@ -48,21 +49,21 @@ const ConsolePage = () => {
     }
   }, [selectedHwid]);
 
-  useEffect(() => {
-    apiClient.clients.helper.initSocket((data) => {
-      setConsoles((prevConsoles) => {
-        return prevConsoles.map((console) =>
-          console.hwid === data.hwid
-            ? { ...console, client: { ...console.client, online: data.online } }
-            : console
-        );
-      });
-    });
+  // useEffect(() => {
+  //   apiClient.clients.helper.initSocket((data) => {
+  //     setConsoles((prevConsoles) => {
+  //       return prevConsoles.map((console) =>
+  //         console.hwid === data.hwid
+  //           ? { ...console, client: { ...console.client, online: data.online } }
+  //           : console
+  //       );
+  //     });
+  //   });
 
-    return () => {
-      apiClient.clients.helper.disconnectSocket();
-    };
-  }, []);
+  //   return () => {
+  //     apiClient.clients.helper.disconnectSocket();
+  //   };
+  // }, []);
 
   const closeConsole = (hwid: string) => {
     if (!confirmClose[hwid]) {
@@ -98,8 +99,6 @@ const ConsolePage = () => {
   const sendCommand = (hwid: string, command: string) => {
     apiClient.clients.helper.sendCommand(hwid, command).then((response) => {
       if (response.status) {
-        toast.success("Command sent successfully");
-
         const newMessage: Messages = {
           content: command,
           response: response.data.output,
@@ -113,97 +112,121 @@ const ConsolePage = () => {
     });
   };
 
-  const renderTabTitle = (title: string, hwid: string) => {
-    const isOnline = consoles.find((console) => console.hwid === hwid)?.client
-      ?.online;
-
-    return (
-      <div className="flex items-center capitalize">
-        {title}
-        <Chip
-          className="border-none"
-          color={isOnline ? "success" : "danger"}
-          size="sm"
-          variant="dot"
-        />
-        <Icon
-          icon="mdi:close"
-          width={16}
-          height={16}
-          style={{
-            cursor: "pointer",
-            color: confirmClose[hwid] ? "red" : "black",
-          }}
-          onClick={(e) => {
-            e.stopPropagation();
-            closeConsole(hwid);
-          }}
-        />
-      </div>
-    );
-  };
-
   return (
-    <div className="bg-white p-4 rounded-xl shadow-md h-full max-h-[565px] flex flex-col">
+    <div className="flex bg-white rounded-xl shadow-md h-full max-h-[650px]">
       {isLoading ? (
-        <div className="flex justify-center items-center h-full">
+        <div className="flex justify-center items-center h-full w-full">
           <Spinner label="Loading..." />
         </div>
       ) : consoles.length > 0 ? (
         <>
-          <Tabs
-            className="flex justify-center items-center"
-            onSelectionChange={(hwid) => setSelectedHwid(hwid.toString())}
-          >
+          {/* Tabs */}
+          <div className="flex flex-col justify-start bg-white rounded-tl-xl rounded-bl-xl max-w-[250px] h-full p-4 border-r overflow-y-auto custom-scrollbar">
             {consoles.map((console) => (
-              <Tab
+              <div
                 key={console.hwid}
-                title={renderTabTitle(console.name, console.hwid)}
-                value={console.hwid}
-              />
-            ))}
-          </Tabs>
-          <div className="mt-12 overflow-y-auto">
-            {messages.length > 0 ? (
-              messages.map((message, index) => (
-                <div key={index} className="mb-2 flex flex-col">
-                  <div className="flex justify-end">
-                    <p className="text-sm bg-blue-500 text-white p-2 rounded-lg">
-                      <strong>Command:</strong> {message.content}
-                    </p>
-                  </div>
-                  <div className="flex justify-start mt-1">
-                    <p className="text-sm bg-gray-200 p-2 rounded-lg">
-                      <strong>Response:</strong> {message.response}
-                    </p>
-                  </div>
-                  {message.timestamp && (
-                    <p className="text-xs text-gray-500 mt-1">
-                      <strong>Timestamp:</strong>{" "}
-                      {new Date(message.timestamp).toLocaleString()}
-                    </p>
-                  )}
+                className={clsx(
+                  "mb-2 bg-slate-100 p-4 rounded-xl flex items-center cursor-pointer",
+                  {
+                    "bg-slate-200 shadow-md": selectedHwid === console.hwid,
+                  }
+                )}
+                onClick={() => setSelectedHwid(console.hwid)}
+              >
+                <Avatar
+                  size="md"
+                  className="flex justify-start"
+                  classNames={{
+                    base: "bg-gradient-to-br from-[#006bff] to-[#00aaff]",
+                    icon: "text-black/80",
+                  }}
+                  icon={
+                    <Icon
+                      icon="mdi:account"
+                      className="text-black"
+                      fontSize={25}
+                    />
+                  }
+                />
+                <div className="ml-2">
+                  <p className="ml-2 text-ellipsis overflow-hidden whitespace-nowrap">
+                    {console.name}
+                  </p>
+                  <Chip
+                    className="capitalize border-none"
+                    color={console.client?.online ? "success" : "danger"}
+                    size="md"
+                    variant="dot"
+                  >
+                    {console.client?.online ? "online" : "offline"}
+                  </Chip>
                 </div>
-              ))
-            ) : (
-              <p>No messages available</p>
-            )}
+              </div>
+            ))}
           </div>
-          <div className="flex-grow" />
 
-          <div className="flex justify-start mb-4">
-            <Input
-              placeholder="Enter your command"
-              className="w-1/3"
-              value={commandInput}
-              onChange={(e) => setCommandInput(e.target.value)}
-              onKeyPress={(e) => {
-                if (e.key === "Enter") {
-                  sendCommand(selectedHwid!, commandInput);
-                  setCommandInput("");
-                }
-              }}
-            />
+          {/* Console */}
+          <div className="flex-1 p-4 overflow-y-auto custom-scrollbar flex flex-col">
+            {selectedHwid ? (
+              <div className="flex-1">
+                {messages.length > 0 ? (
+                  messages.map((message, index) => (
+                    <div key={index} className="mb-2 flex flex-col">
+                      {/* Sended Command */}
+                      <div className="flex justify-end">
+                        <p className="text-sm bg-blue-500 text-white py-2 px-6 rounded-lg max-w-[50%]">
+                          {message.content}
+                        </p>
+                      </div>
+                      {/* Response */}
+                      <div className="flex justify-start mt-1">
+                        <p className="text-sm bg-gray-200 p-2 rounded-lg max-w-[50%]">
+                          {message.response}
+                        </p>
+                      </div>
+                      {/* Timestamp */}
+                      {message.timestamp && (
+                        <p className="text-xs text-gray-500 mt-1">
+                          <strong>Timestamp:</strong>{" "}
+                          {new Date(message.timestamp).toLocaleString()}
+                        </p>
+                      )}
+                    </div>
+                  ))
+                ) : (
+                  <div className="flex justify-center items-center h-full">
+                    <h1 className="font-semibold text-2xl">
+                      Right now there are no commands in the history for this
+                      console
+                    </h1>
+                  </div>
+                )}
+              </div>
+            ) : (
+              <div className="flex justify-center items-center h-full">
+                <h1 className="font-semibold text-2xl">Select a console</h1>
+              </div>
+            )}
+
+            {/* Input and Button Container */}
+            <div className="flex justify-center items-center p-4 border-t">
+              <Input
+                value={commandInput}
+                onChange={(e) => setCommandInput(e.target.value)}
+                placeholder="Enter command"
+                className="mr-2"
+              />
+              <Button
+                onPress={() => {
+                  if (selectedHwid && commandInput) {
+                    sendCommand(selectedHwid, commandInput);
+                    setCommandInput("");
+                  }
+                }}
+              >
+                Send
+              </Button>
+            </div>
           </div>
         </>
       ) : (
