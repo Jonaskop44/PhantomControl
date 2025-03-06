09ad90835a299ab02da6e6644f3747abf99ec6ff
jonaskop44 <jonas@codeflexx.com>
2025-03-06 21:48:24 +0100

    changes

--- server/src/stripe/stripe.controller.ts
+++ server/src/stripe/stripe.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Query,
+  Request,
+  UseGuards,
+  BadRequestException,
+} from '@nestjs/common';
 import { StripeService } from './stripe.service';
 import { JwtGuard } from 'src/guard/jwt.guard';
 
@@ -10,5 +18,22 @@ export class StripeController {
   @Get('products')
   async getProducts() {
     return this.stripeService.getProducts();
+  }
+
+  @Post('create-checkout-session')
+  async createCheckoutSession(
+    @Request() request,
+    @Query('plan') planName: string,
+  ) {
+    if (!planName) throw new BadRequestException('Plan is required');
+
+    return this.stripeService.createCheckoutSession(request, planName);
+  }
+
+  @Get('session-status')
+  async getSessionStatus(@Query('session_id') sessionId: string) {
+    if (!sessionId) throw new BadRequestException('Session ID is required');
+
+    return this.stripeService.getSessionStatus(sessionId);
   }
 }
--- server/src/stripe/stripe.service.ts
+++ server/src/stripe/stripe.service.ts
@@ -1,4 +1,11 @@
-import { Inject, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Inject,
+  Injectable,
+  InternalServerErrorException,
+  NotFoundException,
+} from '@nestjs/common';
+import { Request } from 'express';
 import Stripe from 'stripe';
 
 @Injectable()
@@ -19,5 +26,61 @@ export class StripeService {
     );
     console.log(mian);
     return (await this.stripe.products.list()).data;
+  }
+
+  async createCheckoutSession(request: Request, planName: string) {
+    try {
+      const planId = await this.stripe.products.search({
+        query: `name:'${planName}'`,
+        expand: ['data.default_price'],
+      });
+
+      if (!planId.data.length) {
+        throw new ConflictException('Plan not found');
+      }
+
+      const product = planId.data[0];
+
+      if (!product.default_price) {
+        throw new ConflictException('Price not found');
+      }
+
+      const priceId =
+        typeof product.default_price === 'string'
+          ? product.default_price
+          : product.default_price.id;
+
+      const session = await this.stripe.checkout.sessions.create({
+        ui_mode: 'embedded',
+        payment_method_types: ['card'],
+        line_items: [
+          {
+            price: priceId,
+            quantity: 1,
+          },
+        ],
+        mode: 'subscription',
+        return_url: `${request.headers.origin}/return?session_id={CHECKOUT_SESSION_ID}`,
+      });
+
+      return { sessionId: session.id, client_secret: session.client_secret };
+    } catch (error) {
+      console.log('[STRIPE createCheckoutSession]: ', error);
+      throw new ConflictException('Error creating checkout session');
+    }
+  }
+
+  async getSessionStatus(sessionId: string) {
+    try {
+      const session = await this.stripe.checkout.sessions.retrieve(sessionId);
+
+      return { status: session.payment_status, customer: session.customer };
+    } catch (error) {
+      if (error.type === 'StripeInvalidRequestError')
+        throw new NotFoundException('Session not found');
+
+      console.log('[STRIPE getSessionStatus]: ', error);
+      throw new InternalServerErrorException('Error getting session status');
+    }
   }
 }
